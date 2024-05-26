027d53b9b609bc818d1d83561110b53a528deaba
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 13:07:39 +0600

    Joining Array

---
+++ Coding_With_Mosh/Array/Joining_Array/index.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const joined = numbers.join(',');
+console.log( joined );
+
+const message  = "This is my first message";
+const combined = message.split(' ');
+const joinded  = combined.join('_');
+console.log(combined);
+console.log( joinded );
