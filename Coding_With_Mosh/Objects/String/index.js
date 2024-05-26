d24332f31fde9b8eb6a5b09dd469fdb68fedf13e
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:57:19 +0600

    String

---
+++ Coding_With_Mosh/Objects/String/index.js
@@ -0,0 +1,22 @@
+// String Primitives 
+
+const message = "This is my first message";
+// console.log(typeof message);
+// console.log(message.length);
+// console.log(message[0]);
+// console.log(message[1]);
+// console.log(message.includes('first'));
+// console.log(message.includes('not'));
+// console.log(message.startsWith('This'));
+// console.log(message.startsWith('this'));
+// console.log(message.endsWith('e'));
+// console.log(message.indexOf('my'));
+// console.log(message.replace('first', 'second'));
+// console.log(message.toUpperCase());
+// console.log(message.toLowerCase());
+// console.log(message.split(" "));
+
+// String Object
+
+const another = new String('Hi');
+// console.log(typeof another);
