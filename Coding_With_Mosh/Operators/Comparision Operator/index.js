d3911785b4f668b94773ebfe18cc3914f5e54b40
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:26:02 +0600

    Comparison Operator

---
+++ Coding_With_Mosh/Operators/Comparision Operator/index.js
@@ -0,0 +1,18 @@
+let x = 1;
+
+// Relations Operator
+
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+
+// Equal Operators
+
+
+console.log(x === 1);
+console.log(x !== 1);
+console.log(x == 1);
+console.log(x = 2);
+
