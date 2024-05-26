3612d41754a0ba470137341b7de38125ef098486
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:20:31 +0600

    Object

---
+++ Coding_With_Mosh/Basics/Object/index.js
@@ -0,0 +1,16 @@
+let person = {
+    name : 'Ashikur Rahman',
+    age : 30
+}
+
+// Dot Notation ... 
+
+person.name = 'John';
+console.log(person.name);
+
+// Brackets Notation ... 
+
+let selection = 'age';
+person[selection] = 40;
+
+console.log(person.age);
