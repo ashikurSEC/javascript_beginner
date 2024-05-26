79ff1a6f5f131389857f36a3f229f9ee360feb0b
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:16:25 +0600

    Constatant

---
+++ Coding_With_Mosh/Basics/constant/index.js
@@ -0,0 +1,13 @@
+// The value of let is immutable
+
+let priceRate = 0.2;
+priceRate = 1;
+console.log(priceRate);
+
+
+//The value of const is immutable 
+const  interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+
