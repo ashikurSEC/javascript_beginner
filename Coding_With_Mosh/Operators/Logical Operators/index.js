697e328bbac3c4af6f51c4e2ddd1c2f2c8ccf5b0
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:28:26 +0600

    Logical Operators

---
+++ Coding_With_Mosh/Operators/Logical Operators/index.js
@@ -0,0 +1,14 @@
+// Logical OR (||)
+// Return TRUE if one of the operands TRUE
+
+let highIncome = true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome || goodCreditScore;
+
+console.log( eligibleForLoan );
+
+// NOT ( ! )
+
+let applicationRefused = !eligibleForLoan;
+
+console.log( applicationRefused );
