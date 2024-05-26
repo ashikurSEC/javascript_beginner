e3db3aa14b7117827b695ef9a7ccf0eb9814329c
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 13:05:48 +0600

    Combing Slicing Arrays

---
+++ Coding_With_Mosh/Array/Combing_Slicing_Arrays/index.js
@@ -0,0 +1,9 @@
+const first  = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat( second );
+console.log( combined );
+
+
+const slice = combined.slice(2, 4);
+console.log( slice );
