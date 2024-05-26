712b2f83746f808d130c1be8647ce7ece00a2efa
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 13:02:05 +0600

    Finding Elements Primitives

---
+++ Coding_With_Mosh/Array/Finding_Element_Primitives/index.js
@@ -0,0 +1,7 @@
+const number = [1, 2, 3, 4, 5];
+
+console.log(number.indexOf(2)); // Return index Position
+console.log(number.indexOf('a')); // return -1
+console.log(number.indexOf(1) !== -1); return //boolean true
+console.log(number.includes(6)); // return boolean false
+console.log(number.includes(1)); // return boolean true
