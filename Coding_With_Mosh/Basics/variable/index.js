da664651fb62a3fe110f5f85819cdd58d9e42daf
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:15:04 +0600

    Variable

---
+++ Coding_With_Mosh/Basics/variable/index.js
@@ -0,0 +1,12 @@
+
+let name = 'Ashikur Rahman';
+console.log(name);
+
+// Cannot be reserved keyword
+// Should be meaningful
+// Cannot start with a number (1number)
+// Cannot contain a space or hypen (-)
+// Using camolcase
+
+let firstName = 'Ashikur Rahman';
+console.log(firstName);
