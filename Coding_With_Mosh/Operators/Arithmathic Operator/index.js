6698e67ed97cedc83f4856aa88b1bf77f0f18e6b
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:23:49 +0600

    Arithmathic Operator

---
+++ Coding_With_Mosh/Operators/Arithmathic Operator/index.js
@@ -0,0 +1,16 @@
+let x = 30;
+let y = 20;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+// increment 
+console.log(++x);
+
+// decrement 
+
+console.log(--x);
