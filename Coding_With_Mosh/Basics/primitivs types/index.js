0a0f1a0e5dd581880b430c2514f1d2f211bc29ac
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:17:45 +0600

    Primitives Types

---
+++ Coding_With_Mosh/Basics/primitivs types/index.js
@@ -0,0 +1,21 @@
+//^ Primitives types / Value Types 
+/*
+    1. string
+
+    2. number 
+
+    3. boolean
+
+    4. undefind
+
+    5. null
+
+*/
+    let name    = 'Ashikur Rahman'; // String Litaral
+    let age     = 30; // Number Litaral
+    let isApproved = true; // Boolean Litaral
+    let firstName; // undefind
+    let selectedColor = null; // null
+
+
+    console.log(firstName);
