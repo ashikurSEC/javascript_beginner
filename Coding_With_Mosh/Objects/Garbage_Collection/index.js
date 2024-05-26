33e7e38881639fd48aa6552ae9faf28a08ae216d
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:56:04 +0600

    Garbage Collection

---
+++ Coding_With_Mosh/Objects/Garbage_Collection/index.js
@@ -0,0 +1,18 @@
+let user = {};
+console.log( user );
+
+// function createCircularReference() {
+//     let obj1 = {};
+//     let obj2 = {};
+
+//     obj1.reference = obj2;
+//     obj2.reference = obj1;
+
+
+
+//     obj1 = null;
+//     obj2 = null;
+// }
+
+
+// createCircularReference();
