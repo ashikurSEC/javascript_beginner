cce8b3a48be782b32e764eba5d582210067e2b63
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:56:47 +0600

    Math

---
+++ Coding_With_Mosh/Objects/Math/index.js
@@ -0,0 +1,27 @@
+//& Math.random...
+
+let randomNumber = 5;
+console.log(Math.random( randomNumber ));
+
+//! Math.round...
+
+let round_number = 4.49;
+console.log(Math.round( round_number ));
+
+
+//~ Math.floor... 
+
+let floor_number = -4.9;
+console.log(Math.floor( floor_number ));
+
+
+//* Math.max...
+
+let max_number = [10, 20, 30, 40, 50];
+console.log(Math.max( ...max_number ));
+
+
+//! Math.min...
+
+let min_number = [10, -2, 30, 40, 50,];
+console.log(Math.min(...min_number));
