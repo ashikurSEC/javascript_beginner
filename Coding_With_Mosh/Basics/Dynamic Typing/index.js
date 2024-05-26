af891c07b1a2db3d788d94d1c4f48d7c02498c8e
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-26 12:18:43 +0600

    Dynamic Typing

---
+++ Coding_With_Mosh/Basics/Dynamic Typing/index.js
@@ -0,0 +1,19 @@
+/*
+
+                        Static      <------- Language ------> Dynamic
+                        string name = 'Jhon';         let name = 'Jhon';
+
+                        
+*/
+
+let name = 'Ashikur';
+let age = 30;
+let isApproved = true;
+let firstName ;
+let selectedColor = null;
+
+console.log(typeof name);
+console.log(typeof age);    
+console.log(typeof isApproved);
+console.log(typeof firstName);
+console.log(typeof selectedColor);
