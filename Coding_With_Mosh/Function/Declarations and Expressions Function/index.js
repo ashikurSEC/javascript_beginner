32807eab7e1d7e1bf46bec3e1f4ce3943a3cefd0
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-30 22:08:49 +0600

    Declarations and Expressions Functions

---
+++ Coding_With_Mosh/Function/Declarations and Expressions Function/index.js
@@ -0,0 +1,15 @@
+// Declarations Function... 
+
+function myFunction () {
+    console.log('Hello John!');
+}
+
+myFunction();
+
+// Anonymous Function Expressions...
+
+let run = function () {
+    console.log('Hello World!');
+};
+
+run();
