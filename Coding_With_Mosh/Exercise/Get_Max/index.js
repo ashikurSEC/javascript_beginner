5601247ab853593ebd5e2b9837cbe3ab8f63e244
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-30 21:46:45 +0600

    reduce using

--- Coding_With_Mosh/Exercise/Get_Max/index.js
+++ Coding_With_Mosh/Exercise/Get_Max/index.js
@@ -6,16 +6,18 @@ console.log( max );
 
 
 function getMax ( array ) {
-    if ( array.length === 0 ) {
-        return undefined;
-    }
+    // if ( array.length === 0 ) {
+    //     return undefined;
+    // }
 
-    let max = array[0];
+    // let max = array[0];
 
-    for (let i = 1; i <= array.length; i++) {
-        if (array[i] > max) {
-            max = array[i];
-        }
-    }
-    return max;
+    // for (let i = 1; i <= array.length; i++) {
+    //     if (array[i] > max) {
+    //         max = array[i];
+    //     }
+    // }
+    // return max;
+
+    return array.reduce((a, b) => (a > b) ? a : b);
 }
