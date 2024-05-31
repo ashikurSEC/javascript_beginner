fd0de128916e456e54a5e95e5b069718cbb20591
ashikurSEC <ashikurrahman7194@gmail.com>
2024-05-31 11:54:48 +0600

    change array value

--- Coding_With_Mosh/Function/The this keyword/index.js
+++ Coding_With_Mosh/Function/The this keyword/index.js
@@ -1,6 +1,6 @@
 const video = {
     title : 'X-men',
-    part  : ['1', '2', '3'],
+    part  : [1, 2, 3],
     showTags () {
         this.part.forEach(function (part) {
             console.log(this.title, part);
